3bbfd28481267a3055104a2890859d017afaf427
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-18 14:13:39 +0100

    chore: change mode to assertion, change file name
    chore: change mode to assertion

---
+++ src/TestNaughtCoin.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// comment out this line and Echidna works
+import "./NaughtCoin.sol";
+
+contract TestNaughtCoin {
+    uint256 counter;
+
+    // NaughtCoin token;
+
+    function increment() public {
+        counter++;
+    }
+
+    function simple_test() public {
+        assert(counter != 2);
+    }
+}
