f643e93448ddeeeecebed37873ab1ab2ac18fdef
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-01-18 13:51:48 +0100

    chore: simplify example

--- src/EchidnaNaughtCoin.sol
+++ src/EchidnaNaughtCoin.sol
@@ -4,13 +4,6 @@ pragma solidity ^0.8.0;
 import {NaughtCoin} from "./NaughtCoin.sol";
 
 contract EchidnaNaughtCoin {
-    NaughtCoin token;
-    address echidna_caller = msg.sender;
-
-    constructor() {
-        token = new NaughtCoin(echidna_caller);
-    }
-
     function echidna_always_true() public returns (bool) {
         return (true);
     }
